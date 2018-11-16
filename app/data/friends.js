f5839e3b09020ec05a248e91aa75639dba649792
AlexanderPhan <42187090+AlexanderPhan@users.noreply.github.com>
2018-11-15 20:15:38 -0600

    index.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
 const path = require("path");
-var raw = fs.readFileSync(path.join(__dirname, "friends.json"));
+var raw = fs.readFileSync(path.join(__dirname, "package.json"));
 var friends = JSON.parse(raw);
 module.exports = friends;
